918199d602f48c8903bd129428a5b70a010d670b
Jeison0770 <jeisonparedes63@gmail.com>
2023-06-10 00:20:55 -0500

    cliente Listo

--- TALLER#3/Cliente.Designer.vb
+++ TALLER#3/Cliente.Designer.vb
@@ -22,34 +22,269 @@ Partial Class Cliente
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As DataGridViewCellStyle = New DataGridViewCellStyle()
         Label1 = New Label()
+        DataGridView1 = New DataGridView()
+        ActualizarBtn = New Button()
+        BtnIngresar = New Button()
+        ModificarClientesPanel = New Panel()
+        EstadoCb = New ComboBox()
+        ObservacionTb = New TextBox()
+        Label4 = New Label()
+        LabelTítulo = New Label()
+        CostoTb = New TextBox()
+        Label3 = New Label()
+        IdClienteTb = New TextBox()
+        PanelAutorExistente = New Panel()
+        ListaAutorExistente = New ComboBox()
+        LabelVe = New Label()
+        Label2 = New Label()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        ModificarClientesPanel.SuspendLayout()
+        PanelAutorExistente.SuspendLayout()
         SuspendLayout()
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
         Label1.Enabled = False
-        Label1.Font = New Font("Montserrat", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
         Label1.ForeColor = Color.FromArgb(CByte(120), CByte(127), CByte(130))
-        Label1.Location = New Point(173, 64)
+        Label1.Location = New Point(13, 9)
         Label1.Margin = New Padding(4, 0, 4, 0)
         Label1.Name = "Label1"
-        Label1.Size = New Size(116, 32)
+        Label1.Size = New Size(122, 29)
         Label1.TabIndex = 12
         Label1.Text = "CLIENTE"
         ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.BackgroundColor = Color.WhiteSmoke
+        DataGridView1.BorderStyle = BorderStyle.None
+        DataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle1.BackColor = Color.White
+        DataGridViewCellStyle1.Font = New Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        DataGridViewCellStyle1.ForeColor = Color.Red
+        DataGridViewCellStyle1.Padding = New Padding(5)
+        DataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = DataGridViewTriState.True
+        DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        DataGridView1.ColumnHeadersHeight = 40
+        DataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle2.BackColor = SystemColors.Window
+        DataGridViewCellStyle2.Font = New Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        DataGridViewCellStyle2.ForeColor = Color.FromArgb(CByte(120), CByte(127), CByte(130))
+        DataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = DataGridViewTriState.False
+        DataGridView1.DefaultCellStyle = DataGridViewCellStyle2
+        DataGridView1.Location = New Point(12, 255)
+        DataGridView1.Margin = New Padding(4, 3, 4, 3)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.RowTemplate.Height = 31
+        DataGridView1.Size = New Size(1034, 250)
+        DataGridView1.TabIndex = 53
+        ' 
+        ' ActualizarBtn
+        ' 
+        ActualizarBtn.BackColor = Color.FromArgb(CByte(229), CByte(229), CByte(229))
+        ActualizarBtn.Cursor = Cursors.Hand
+        ActualizarBtn.Enabled = False
+        ActualizarBtn.FlatAppearance.BorderColor = Color.FromArgb(CByte(120), CByte(127), CByte(130))
+        ActualizarBtn.FlatAppearance.BorderSize = 0
+        ActualizarBtn.FlatStyle = FlatStyle.Flat
+        ActualizarBtn.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        ActualizarBtn.ForeColor = Color.FromArgb(CByte(83), CByte(97), CByte(98))
+        ActualizarBtn.Location = New Point(201, 148)
+        ActualizarBtn.Name = "ActualizarBtn"
+        ActualizarBtn.Size = New Size(144, 49)
+        ActualizarBtn.TabIndex = 52
+        ActualizarBtn.Text = "Actualizar"
+        ActualizarBtn.UseVisualStyleBackColor = False
+        ' 
+        ' BtnIngresar
+        ' 
+        BtnIngresar.BackColor = Color.FromArgb(CByte(229), CByte(229), CByte(229))
+        BtnIngresar.Cursor = Cursors.Hand
+        BtnIngresar.FlatAppearance.BorderColor = Color.FromArgb(CByte(120), CByte(127), CByte(130))
+        BtnIngresar.FlatAppearance.BorderSize = 0
+        BtnIngresar.FlatStyle = FlatStyle.Flat
+        BtnIngresar.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        BtnIngresar.ForeColor = Color.FromArgb(CByte(83), CByte(97), CByte(98))
+        BtnIngresar.Location = New Point(11, 148)
+        BtnIngresar.Name = "BtnIngresar"
+        BtnIngresar.Size = New Size(144, 49)
+        BtnIngresar.TabIndex = 49
+        BtnIngresar.Text = "Ingresar"
+        BtnIngresar.UseVisualStyleBackColor = False
+        ' 
+        ' ModificarClientesPanel
+        ' 
+        ModificarClientesPanel.Controls.Add(EstadoCb)
+        ModificarClientesPanel.Controls.Add(ObservacionTb)
+        ModificarClientesPanel.Controls.Add(Label4)
+        ModificarClientesPanel.Controls.Add(LabelTítulo)
+        ModificarClientesPanel.Controls.Add(CostoTb)
+        ModificarClientesPanel.Controls.Add(Label3)
+        ModificarClientesPanel.Enabled = False
+        ModificarClientesPanel.Location = New Point(383, 38)
+        ModificarClientesPanel.Name = "ModificarClientesPanel"
+        ModificarClientesPanel.Size = New Size(674, 182)
+        ModificarClientesPanel.TabIndex = 50
+        ' 
+        ' EstadoCb
+        ' 
+        EstadoCb.DropDownStyle = ComboBoxStyle.DropDownList
+        EstadoCb.FormattingEnabled = True
+        EstadoCb.Location = New Point(438, 52)
+        EstadoCb.Name = "EstadoCb"
+        EstadoCb.Size = New Size(151, 28)
+        EstadoCb.TabIndex = 21
+        ' 
+        ' ObservacionTb
+        ' 
+        ObservacionTb.Font = New Font("Microsoft Sans Serif", 14.999999F, FontStyle.Bold, GraphicsUnit.Point)
+        ObservacionTb.ForeColor = Color.FromArgb(CByte(83), CByte(97), CByte(98))
+        ObservacionTb.Location = New Point(161, 100)
+        ObservacionTb.Multiline = True
+        ObservacionTb.Name = "ObservacionTb"
+        ObservacionTb.Size = New Size(491, 65)
+        ObservacionTb.TabIndex = 19
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.ForeColor = Color.FromArgb(CByte(120), CByte(127), CByte(130))
+        Label4.Location = New Point(14, 122)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(141, 25)
+        Label4.TabIndex = 20
+        Label4.Text = "Observación:"
+        ' 
+        ' LabelTítulo
+        ' 
+        LabelTítulo.AutoSize = True
+        LabelTítulo.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        LabelTítulo.ForeColor = Color.FromArgb(CByte(120), CByte(127), CByte(130))
+        LabelTítulo.Location = New Point(346, 51)
+        LabelTítulo.Name = "LabelTítulo"
+        LabelTítulo.Size = New Size(86, 25)
+        LabelTítulo.TabIndex = 16
+        LabelTítulo.Text = "Estado:"
+        ' 
+        ' CostoTb
+        ' 
+        CostoTb.Font = New Font("Microsoft Sans Serif", 14.999999F, FontStyle.Bold, GraphicsUnit.Point)
+        CostoTb.ForeColor = Color.FromArgb(CByte(83), CByte(97), CByte(98))
+        CostoTb.Location = New Point(96, 44)
+        CostoTb.Name = "CostoTb"
+        CostoTb.Size = New Size(191, 36)
+        CostoTb.TabIndex = 14
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.ForeColor = Color.FromArgb(CByte(120), CByte(127), CByte(130))
+        Label3.Location = New Point(14, 51)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(76, 25)
+        Label3.TabIndex = 18
+        Label3.Text = "Costo:"
+        ' 
+        ' IdClienteTb
+        ' 
+        IdClienteTb.Font = New Font("Microsoft Sans Serif", 14.999999F, FontStyle.Bold, GraphicsUnit.Point)
+        IdClienteTb.ForeColor = Color.FromArgb(CByte(83), CByte(97), CByte(98))
+        IdClienteTb.Location = New Point(154, 59)
+        IdClienteTb.Name = "IdClienteTb"
+        IdClienteTb.Size = New Size(213, 36)
+        IdClienteTb.TabIndex = 47
+        ' 
+        ' PanelAutorExistente
+        ' 
+        PanelAutorExistente.Controls.Add(ListaAutorExistente)
+        PanelAutorExistente.Controls.Add(LabelVe)
+        PanelAutorExistente.Location = New Point(569, 97)
+        PanelAutorExistente.Name = "PanelAutorExistente"
+        PanelAutorExistente.Size = New Size(390, 83)
+        PanelAutorExistente.TabIndex = 51
+        ' 
+        ' ListaAutorExistente
+        ' 
+        ListaAutorExistente.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        ListaAutorExistente.ForeColor = Color.FromArgb(CByte(120), CByte(127), CByte(130))
+        ListaAutorExistente.FormattingEnabled = True
+        ListaAutorExistente.Location = New Point(0, 40)
+        ListaAutorExistente.Name = "ListaAutorExistente"
+        ListaAutorExistente.Size = New Size(391, 33)
+        ListaAutorExistente.TabIndex = 41
+        ' 
+        ' LabelVe
+        ' 
+        LabelVe.AutoSize = True
+        LabelVe.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        LabelVe.ForeColor = Color.FromArgb(CByte(120), CByte(127), CByte(130))
+        LabelVe.Location = New Point(-1, 0)
+        LabelVe.Name = "LabelVe"
+        LabelVe.Size = New Size(64, 25)
+        LabelVe.TabIndex = 42
+        LabelVe.Text = "Autor"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.ForeColor = Color.FromArgb(CByte(120), CByte(127), CByte(130))
+        Label2.Location = New Point(4, 66)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(144, 25)
+        Label2.TabIndex = 48
+        Label2.Text = "ID del cliente:"
+        ' 
         ' Cliente
         ' 
-        AutoScaleDimensions = New SizeF(9.0F, 21.0F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1192, 570)
+        ClientSize = New Size(1060, 543)
+        Controls.Add(DataGridView1)
+        Controls.Add(ActualizarBtn)
+        Controls.Add(BtnIngresar)
+        Controls.Add(ModificarClientesPanel)
+        Controls.Add(IdClienteTb)
+        Controls.Add(PanelAutorExistente)
+        Controls.Add(Label2)
         Controls.Add(Label1)
         FormBorderStyle = FormBorderStyle.None
         Name = "Cliente"
         Text = "CLIENTE"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ModificarClientesPanel.ResumeLayout(False)
+        ModificarClientesPanel.PerformLayout()
+        PanelAutorExistente.ResumeLayout(False)
+        PanelAutorExistente.PerformLayout()
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ActualizarBtn As Button
+    Friend WithEvents BtnIngresar As Button
+    Friend WithEvents ModificarClientesPanel As Panel
+    Friend WithEvents EstadoCb As ComboBox
+    Friend WithEvents ObservacionTb As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents LabelTítulo As Label
+    Friend WithEvents CostoTb As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents IdClienteTb As TextBox
+    Friend WithEvents PanelAutorExistente As Panel
+    Friend WithEvents ListaAutorExistente As ComboBox
+    Friend WithEvents LabelVe As Label
+    Friend WithEvents Label2 As Label
 End Class
--- TALLER#3/Cliente.vb
+++ TALLER#3/Cliente.vb
@@ -6,5 +6,203 @@ Public Class Cliente
     Dim connectionString As String = varGlobales.cadenaConexion
     Private Sub Cliente_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Location = New Point(Form1.Location.X, Form1.Location.Y + 48) ' Establecer la nueva ubicación de Form4 en relación con Form1
+        Try
+
+            conexion.Open()
+
+            DataGridView1.DataSource = MostrarClientes()
+            'PROPIEDADES GRIDVIEW-----------------------------------------------------------------
+            DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            DataGridView1.AutoSize = False
+            DataGridView1.MaximumSize = New Size(1192, 570)
+            DataGridView1.AutoResizeColumns()
+            DataGridView1.ReadOnly = True
+
+            conexion.Close()
+            EstadoCb.Items.Add("Prestado")
+            EstadoCb.Items.Add("Devuelto")
+        Catch ex As Exception
+
+            MsgBox("ERRROR CONECTAR CON BASE")
+            conexion.Close()
+
+        End Try
+    End Sub
+    Public Function MostrarClientes() As DataTable
+        Dim query As String = "SELECT Cliente.Id, Cliente.Nombre AS Cliente, Books.Title AS Libro, Prestamos.Costo, Prestamos.Estado, Prestamos.Observacion, Prestamos.FechaPrestamo, Prestamos.FechaDevolucion
+            FROM Cliente
+            JOIN Prestamos ON Cliente.Id = Prestamos.ClienteId
+            JOIN Books ON Prestamos.LibroId = Books.Id"
+
+        Dim dataTable As New DataTable()
+        ' Crear el adaptador y ejecutar la consulta
+        Dim adapter As New SqlDataAdapter(query, conexion)
+        adapter.Fill(dataTable)
+
+        Return dataTable
+
+    End Function
+
+    Private Sub BtnIngresar_Click(sender As Object, e As EventArgs) Handles BtnIngresar.Click
+        ActualizarBtn.Enabled = True
+        ModificarClientesPanel.Enabled = True
+        Dim IdCliente As Integer = Integer.Parse(IdClienteTb.Text)
+        Using con As New SqlConnection(connectionString)
+            ' Abrir la conexión
+            con.Open()
+
+            ' Consulta SQL para obtener los datos
+            Dim query As String = "SELECT Cliente.Id, Cliente.Nombre AS Cliente, Books.Title AS Libro, Prestamos.Costo, Prestamos.Estado, Prestamos.Observacion, Prestamos.FechaPrestamo, Prestamos.FechaDevolucion
+            FROM Cliente
+            JOIN Prestamos ON Cliente.Id = Prestamos.ClienteId
+            JOIN Books ON Prestamos.LibroId = Books.Id
+            WHERE Cliente.Id = " & IdCliente
+
+            ' Crear un adaptador de datos
+            Dim adapter As New SqlDataAdapter(query, con)
+            ' Crear un DataSet para almacenar los datos
+            Dim dataSet As New DataSet()
+
+            ' Llenar el DataSet con los datos del adaptador
+            adapter.Fill(dataSet, "Cliente")
+
+            ' Asignar el DataTable al control DataGridView
+            DataGridView1.DataSource = dataSet.Tables("Cliente")
+
+            ' Cerrar la conexión
+            con.Close()
+            MostrarCosto(IdCliente)
+            MostrarEstado(IdCliente)
+            MostrarObservacion(IdCliente)
+            IdClienteTb.Enabled = False
+            BtnIngresar.Enabled = False
+        End Using
+    End Sub
+    Private Sub MostrarCosto(ByVal IdCliente As Integer)
+
+        Using con As New SqlConnection(connectionString)
+            con.Open()
+            Dim query As String = "SELECT Costo FROM Prestamos WHERE ClienteId = " & IdCliente
+            Dim command As New SqlCommand(query, con)
+            Dim dato As Object = command.ExecuteScalar()
+
+            If dato IsNot Nothing Then
+                CostoTb.Text = dato.ToString()
+            Else
+                CostoTb.Text = "No se encontró el dato."
+            End If
+        End Using
+    End Sub
+    Private Sub MostrarEstado(ByVal IdCliente As Integer)
+
+        Using con As New SqlConnection(connectionString)
+            con.Open()
+            Dim query As String = "SELECT Estado FROM Prestamos WHERE ClienteId = " & IdCliente
+            Dim command As New SqlCommand(query, con)
+            Dim dato As Object = command.ExecuteScalar()
+
+            If dato IsNot Nothing Then
+                EstadoCb.SelectedItem = dato.ToString()
+            Else
+                EstadoCb.SelectedIndex = -1
+            End If
+        End Using
+    End Sub
+    Private Sub MostrarObservacion(ByVal IdCliente As Integer)
+
+        Using con As New SqlConnection(connectionString)
+            con.Open()
+            Dim query As String = "SELECT Observacion FROM Prestamos WHERE ClienteId = " & IdCliente
+            Dim command As New SqlCommand(query, con)
+            Dim dato As Object = command.ExecuteScalar()
+
+            If dato IsNot Nothing Then
+                ObservacionTb.Text = dato.ToString()
+            Else
+                ObservacionTb.Text = "No se encontró el dato."
+            End If
+        End Using
+    End Sub
+
+    Private Sub ActualizarBtn_Click(sender As Object, e As EventArgs) Handles ActualizarBtn.Click
+        Dim IdCliente As Integer = Integer.Parse(IdClienteTb.Text)
+        ActualizarCosto(IdCliente)
+        ActualizarEstado(IdCliente)
+        ActualizarObservacion(IdCliente)
+        conexion.Open()
+        DataGridView1.DataSource = MostrarClientes()
+        IdClienteTb.Enabled = True
+        BtnIngresar.Enabled = True
+        ActualizarBtn.Enabled = False
+        ModificarClientesPanel.Enabled = False
+    End Sub
+    Private Sub ActualizarCosto(ByVal IdCliente As Integer)
+        ' Obtener el valor del cuadro de texto para el nuevo Costo
+        Dim Costo As Decimal = Decimal.Parse(CostoTb.Text)
+
+        ' Consulta SQL para actualizar la columna Costo
+        Dim sqlQuery As String = "UPDATE Prestamos SET Costo = @Costo WHERE ClienteId = " & IdCliente
+
+        ' Crear una SqlConnection y SqlCommand
+        Using connection As New SqlConnection(connectionString)
+            Using command As New SqlCommand(sqlQuery, connection)
+                ' Agregar parámetros para Costo y ClienteId
+                command.Parameters.AddWithValue("@Costo", Costo)
+                command.Parameters.AddWithValue("@ClienteId", IdCliente)
+
+                ' Abrir la conexión
+                connection.Open()
+
+                ' Ejecutar la consulta
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+    Private Sub ActualizarEstado(ByVal IdCliente As Integer)
+        ' Obtener el valor del cuadro de texto para el nuevo Costo
+        Dim Estado As String = EstadoCb.SelectedItem
+
+        ' Consulta SQL para actualizar la columna Costo
+        Dim sqlQuery As String = "UPDATE Prestamos SET Estado = @Estado WHERE ClienteId = " & IdCliente
+
+        ' Crear una SqlConnection y SqlCommand
+        Using connection As New SqlConnection(connectionString)
+            Using command As New SqlCommand(sqlQuery, connection)
+                ' Agregar parámetros para Costo y ClienteId
+                command.Parameters.AddWithValue("@Estado", Estado)
+                command.Parameters.AddWithValue("@ClienteId", IdCliente)
+
+                ' Abrir la conexión
+                connection.Open()
+
+                ' Ejecutar la consulta
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+    Private Sub ActualizarObservacion(ByVal IdCliente As Integer)
+        ' Obtener el valor del cuadro de texto para el nuevo Costo
+        Dim Observacion As String = ObservacionTb.Text
+
+        ' Consulta SQL para actualizar la columna Costo
+        Dim sqlQuery As String = "UPDATE Prestamos SET Observacion = @Observacion WHERE ClienteId = " & IdCliente
+
+        ' Crear una SqlConnection y SqlCommand
+        Using connection As New SqlConnection(connectionString)
+            Using command As New SqlCommand(sqlQuery, connection)
+                ' Agregar parámetros para Costo y ClienteId
+                command.Parameters.AddWithValue("@Observacion", Observacion)
+                command.Parameters.AddWithValue("@ClienteId", IdCliente)
+
+                ' Abrir la conexión
+                connection.Open()
+
+                ' Ejecutar la consulta
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+
+        ' Opcional: Mostrar un cuadro de mensaje para indicar que la actualización fue exitosa
+        MessageBox.Show("Datos actualizado.")
     End Sub
 End Class
