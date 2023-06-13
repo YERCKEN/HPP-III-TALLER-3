ff921c88e521b88dad82a6a8a1623b205c08d16f
Jeison0770 <jeisonparedes63@gmail.com>
2023-06-12 22:34:53 -0500

    arregle un error

--- TALLER#3/eliminacion.vb
+++ TALLER#3/eliminacion.vb
@@ -1,25 +1,25 @@
 ﻿Public Class eliminacion
     Private Sub eliminacion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Try
-            conexion.Open()
-            query = queryT
-            DataGridView1.DataSource = ObtenerLibrosPrestados()
-            ListaMostrar.Text = "Todos"
-            'PROPIEDADES GRIDVIEW-----------------------------------------------------------------
-            DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
-            DataGridView1.AutoSize = False
-            DataGridView1.MaximumSize = New Size(1192, 250)
-            DataGridView1.AutoResizeColumns()
-            DataGridView1.ReadOnly = True
+        'Try
+        '    conexion.Open()
+        '    query = queryT
+        '    DataGridView1.DataSource = ObtenerLibrosPrestados()
+        '    ListaMostrar.Text = "Todos"
+        '    'PROPIEDADES GRIDVIEW-----------------------------------------------------------------
+        '    DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+        '    DataGridView1.AutoSize = False
+        '    DataGridView1.MaximumSize = New Size(1192, 250)
+        '    DataGridView1.AutoResizeColumns()
+        '    DataGridView1.ReadOnly = True
 
 
-            conexion.Close()
+        '    conexion.Close()
 
-        Catch ex As Exception
+        'Catch ex As Exception
 
-            MsgBox("ERRROR CONECTAR CON BASE")
-            conexion.Close()
+        '    MsgBox("ERRROR CONECTAR CON BASE")
+        '    conexion.Close()
 
-        End Try
+        'End Try
     End Sub
 End Class
